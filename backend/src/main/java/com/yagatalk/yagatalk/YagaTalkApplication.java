60b57969564be80db353d928acc03f62132f8aec
jjagorka <jagr68033@mail.ru>
2023-06-27 12:51:57 +0300

    project init

---
+++ backend/src/main/java/com/yagatalk/yagatalk/YagaTalkApplication.java
@@ -0,0 +1,13 @@
+package com.yagatalk.yagatalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagaTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagaTalkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yagatalk/yagatalk/YagaTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagatalk.yagatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagaTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
