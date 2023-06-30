981f2609cdf9021ac78bf46f427b997eb6cb335e
jjagorka <jagr68033@mail.ru>
2023-06-30 16:19:19 +0300

    project init

---
+++ backend/src/main/java/com/yagatalk/controllers/FirstController.java
@@ -0,0 +1,15 @@
+package com.yagatalk.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> getHealth() {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
