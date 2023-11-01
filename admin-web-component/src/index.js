352ba66667a2ad42c206a7ba760094990793b1b3
jjagorka <jagr68033@mail.ru>
2023-11-01 22:44:17 +0300

    #1 set-up

--- admin-web-component/src/index.js
+++ admin-web-component/src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {AuthProvider} from 'react-oauth2-code-pkce';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import {KEYCLOAK_URL} from "./Config";
+
+const realm = 'yagatalk';
+
+const authConfig = {
+    clientId: 'yagatalk-public-client',
+    authorizationEndpoint: `${KEYCLOAK_URL}/realms/${realm}/protocol/openid-connect/auth`,
+    tokenEndpoint: `${KEYCLOAK_URL}/realms/${realm}/protocol/openid-connect/token`,
+    logoutEndpoint: `${KEYCLOAK_URL}/realms/${realm}/protocol/openid-connect/logout`,
+    redirectUri: 'http://localhost:3000',
+    scope: 'openid',
+    onRefreshTokenExpire: (event) => {
+        if (window.confirm('Session expired. Refresh page to continue using the site?')) {
+            event.login();
+        }
+    }
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthProvider authConfig={authConfig}>
+        <App/>
+    </AuthProvider>
 );
+
 
 reportWebVitals();
