13b0b6592e4557fe058f891a90913f7c07695627
jjagorka <jagr68033@mail.ru>
2023-11-24 18:00:17 +0300

    delete comment into index.js

--- author-console/src/index.js
+++ author-console/src/index.js
@@ -5,22 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import App from "./App";
 import {AuthProvider} from "./auth";
 
-// const realm = 'yagatalk';
-
-// const auth/*Config = {
-//     clientId: 'yagatalk-public-client',
-//     authorizationEndpoint: `${KEYCLOAK_URL}/realms/${realm}/protocol/openid-connect/auth`,
-//     tokenEndpoint: `${KEYCLOAK_URL}/realms/${realm}/protocol/openid-connect/token`,
-//     logoutEndpoint: `${KEYCLOAK_URL}/realms/${realm}/protocol/openid-connect/logout`,
-//     redirectUri: `${window.origin}${BASE_PATH}`,
-//     scope: 'openid',
-//     onRefreshTokenExpire: (event) => {
-//         if (window.confirm('Session expired. Refresh page to continue using the site?')) {
-//             event.login();
-//         }
-//     }
-// };*/
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
         <App/>
